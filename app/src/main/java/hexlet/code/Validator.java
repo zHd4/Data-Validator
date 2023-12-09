0dc8bd6cab93df5068c25abb9dcbe461d89b713e
zHd4 <computersystembase@gmail.com>
2023-12-09 22:38:19 +0100

    Refactor Validator

--- app/src/main/java/hexlet/code/Validator.java
+++ app/src/main/java/hexlet/code/Validator.java
@@ -2,7 +2,7 @@ package hexlet.code;
 
 import hexlet.code.schemas.StringSchema;
 
-public class Validator {
+public final class Validator {
     public StringSchema string() {
         return new StringSchema();
     }
