92e1a49c91c8015df5cc87d809499d15def90400
zHd4 <computersystembase@gmail.com>
2023-12-10 21:04:56 +0100

    Create NumberSchemaTest

---
+++ app/src/test/java/hexlet/code/NumberSchemaTest.java
@@ -0,0 +1,33 @@
+package hexlet.code;
+
+import hexlet.code.schemas.NumberSchema;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class NumberSchemaTest {
+    @Test
+    public void testSchema() {
+        Validator validator = new Validator();
+        NumberSchema schema = validator.number();
+
+        Assertions.assertTrue(schema.isValid(null));
+        Assertions.assertTrue(schema.positive().isValid(null));
+
+        schema.required();
+
+        Assertions.assertFalse(schema.isValid(null));
+        Assertions.assertFalse(schema.isValid("5"));
+        Assertions.assertTrue(schema.isValid(10));
+
+        Assertions.assertFalse(schema.isValid(-10));
+        Assertions.assertFalse(schema.isValid(0));
+
+        schema.range(5, 10);
+
+        Assertions.assertTrue(schema.isValid(5));
+        Assertions.assertTrue(schema.isValid(10));
+
+        Assertions.assertFalse(schema.isValid(4));
+        Assertions.assertFalse(schema.isValid(11));
+    }
+}
