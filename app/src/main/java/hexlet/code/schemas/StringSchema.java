fed08ba1693f8abec3f132a7bd20cd406510c155
zHd4 <computersystembase@gmail.com>
2023-12-10 21:04:28 +0100

    Refactor StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,45 +1,39 @@
 package hexlet.code.schemas;
 
-import java.util.function.Predicate;
-
 @SuppressWarnings("UnusedReturnValue")
 public class StringSchema extends BaseSchema {
     public final StringSchema required() {
-        Predicate<Object> condition = value -> {
+        addCondition(value -> {
             if (isString(value)) {
                 return !((String) value).isBlank();
             }
             return false;
-        };
+        });
 
-        addCondition(condition);
         return this;
     }
 
     public final StringSchema minLength(int length) {
-        Predicate<Object> condition = value -> {
+        addCondition(value -> {
             if (isString(value)) {
                 String str = (String) value;
                 return str.length() >= length;
             }
             return false;
-        };
+        });
 
-        addCondition(condition);
         return this;
     }
 
     public final StringSchema contains(String substring) {
-        Predicate<Object> condition = value -> {
+        addCondition(value -> {
             if (isString(value)) {
                 String str = (String) value;
                 return str.contains(substring);
             }
-
             return false;
-        };
+        });
 
-        addCondition(condition);
         return this;
     }
 
