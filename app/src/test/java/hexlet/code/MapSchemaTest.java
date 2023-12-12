58c470f67a33c8dce6090df729bf11fea5f6ac57
zHd4 <computersystembase@gmail.com>
2023-12-12 02:05:50 +0100

    Implement MapSchemaTest.testShape2

--- app/src/test/java/hexlet/code/MapSchemaTest.java
+++ app/src/test/java/hexlet/code/MapSchemaTest.java
@@ -73,4 +73,66 @@ public class MapSchemaTest {
 
         Assertions.assertFalse(schema.isValid(human4));
     }
+
+    @Test
+    public void testShape2() {
+        Validator validator = new Validator();
+        MapSchema schema = validator.map();
+
+        Map<String, BaseSchema> schemas = new HashMap<>();
+
+        schemas.put("title", validator.string().required().minLength(8).contains("island"));
+        schemas.put("text", validator.string().required().minLength(20));
+        schemas.put("number", validator.number().positive());
+
+        schema.shape(schemas);
+
+        Map<String, Object> article1 = new HashMap<>();
+
+        article1.put("title", "Santorini islands");
+        article1.put("text", "Lorem ipsum dolor sit amet");
+        article1.put("number", 1); //true
+
+        Assertions.assertTrue(schema.isValid(article1));
+
+        Map<String, Object> article2 = new HashMap<>();
+
+        article2.put("title", "Mauritius islands");
+        article2.put("text", "Donec venenatis mauris");
+        article2.put("number", 2); //true
+
+        Assertions.assertTrue(schema.isValid(article2));
+
+        Map<String, Object> article3 = new HashMap<>();
+
+        article3.put("title", "Madagascar");
+        article3.put("text", "Donec venenatis mauris");
+        article3.put("number", 2); //false
+
+        Assertions.assertFalse(schema.isValid(article3));
+
+        Map<String, Object> article4 = new HashMap<>();
+
+        article4.put("title", "island");
+        article4.put("text", "Donec venenatis mauris");
+        article4.put("number", 2); //false
+
+        Assertions.assertFalse(schema.isValid(article4));
+
+        Map<String, Object> article5 = new HashMap<>();
+
+        article5.put("title", "Palawan islands");
+        article5.put("text", "Donec");
+        article5.put("number", 2); //false
+
+        Assertions.assertFalse(schema.isValid(article5));
+
+        Map<String, Object> article6 = new HashMap<>();
+
+        article6.put("title", "Bora Bora islands");
+        article6.put("text", "Donec venenatis mauris");
+        article6.put("number", 0); //false
+
+        Assertions.assertFalse(schema.isValid(article6));
+    }
 }
