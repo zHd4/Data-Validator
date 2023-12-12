0ae837bb4bf45e2312c862ace29fa97b706796c6
zHd4 <computersystembase@gmail.com>
2023-12-12 01:38:19 +0100

    Implement MapSchema.shape

--- app/src/main/java/hexlet/code/schemas/MapSchema.java
+++ app/src/main/java/hexlet/code/schemas/MapSchema.java
@@ -1,7 +1,9 @@
 package hexlet.code.schemas;
 
+import java.util.List;
 import java.util.Map;
 import java.util.Objects;
+import java.util.function.Predicate;
 
 @SuppressWarnings("UnusedReturnValue")
 public final class MapSchema extends BaseSchema {
@@ -12,6 +14,27 @@ public final class MapSchema extends BaseSchema {
 
     public MapSchema sizeof(int size) {
         addCondition(entity -> ((Map<?, ?>) entity).size() == size);
+        return this;
+    }
+
+    @SuppressWarnings("unchecked")
+    public MapSchema shape(Map<String, BaseSchema> schemas) {
+        addCondition(data -> {
+            Map<String, Object> map = (Map<String, Object>) data;
+
+            Predicate<Map.Entry<String, Object>> isNotValid = entry -> {
+                BaseSchema schema = schemas.get(entry.getKey());
+                return !schema.isValid(entry.getValue());
+            };
+
+            List<Map.Entry<String, Object>> notValid = map.entrySet()
+                    .stream()
+                    .filter(isNotValid)
+                    .toList();
+
+            return notValid.isEmpty();
+        });
+
         return this;
     }
 }
