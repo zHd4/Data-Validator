4c2739ea22bc455fca34cc07f90f04b88a187b47
zHd4 <computersystembase@gmail.com>
2023-12-14 16:56:13 +0100

    Update StringSchema

--- app/src/main/java/hexlet/code/schemas/StringSchema.java
+++ app/src/main/java/hexlet/code/schemas/StringSchema.java
@@ -1,38 +1,44 @@
 package hexlet.code.schemas;
 
+import java.util.function.Predicate;
+
 @SuppressWarnings("UnusedReturnValue")
 public class StringSchema extends BaseSchema {
     public final StringSchema required() {
-        addCondition(value -> {
+        Predicate<Object> condition = value -> {
             if (isString(value)) {
                 return !((String) value).isBlank();
             }
             return false;
-        });
+        };
 
+        addCondition("required", condition);
         return this;
     }
 
     public final StringSchema minLength(int length) {
-        addCondition(value -> {
+        Predicate<Object> condition = value -> {
             if (isString(value)) {
                 String str = (String) value;
                 return str.length() >= length;
             }
             return false;
-        });
+        };
 
+        addCondition("minLength", condition);
         return this;
     }
 
     public final StringSchema contains(String substring) {
-        addCondition(value -> {
+        Predicate<Object> condition = value -> {
             if (isString(value)) {
                 String str = (String) value;
                 return str.contains(substring);
             }
             return false;
-        });
+        };
+
+        addCondition("contains", condition);
 
         return this;
     }
